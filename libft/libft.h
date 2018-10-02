1c65850f6c3066208e57c8532b9a4db93ca5ca9a
mdchane <mohamed.dchane@outlook.fr>
2018-10-02 18:35:21 +0200

    ok

--- libft/libft.h
+++ libft/libft.h
@@ -4,11 +4,11 @@
  #include <string.h>
  size_t				ft_strlen(const char *s);
  char				*ft_strdup(const char *s1);
- char				*ft_strcpy(char *dst, const char *src);
- char				*ft_strncpy(char *dst, const char *src, size_t n);
+ char				*ft_strcpy(char *dest, const char *src);
+ char				*ft_strncpy(char *dest, const char *src, size_t n);
  char				*ft_strcat(char *s1, const char *s2);
  char				*ft_strncat(char *s1, const char *s2, size_t n);
- size_t				ft_strlcat(char *dst, const char *src, size_t size);
+ size_t				ft_strlcat(char *dest, const char *src, size_t size);
  char				*ft_strchr(const char *s, int c);
  char				*ft_strrchr(const char *s, int c);
  char				*ft_strstr(const char *s1, const char *s2);
--- libft/main.c
+++ libft/main.c
@@ -3,18 +3,33 @@
 
 int		main(int argc, char **argv)
 {
-	char *str1 = "salutggzgezc";
-	char *str2 = "cou";
+	char str1[6] = "salut";
+	char str2[7] = "coucou";
 
 //ft_strlen
 	printf("ft_strlen : %ld\n", ft_strlen(str1));
 	printf("strlen : %ld\n", strlen(str1));
-
+	printf("--------------------------------\n");
 //ft_strcpy
-	printf("ft_strcpy : %s\n", str1);
-	ft_strcpy(str1, str2);
-	//printf("strcpy : %s\n", strcpy(str1, str2));
+	printf("ft_strcpy : %s\n", ft_strcpy(str1, str2));
+	char str1b[6] = "salut";
+	printf("strcpy : %s\n", strcpy(str1b, str2));
+	printf("--------------------------------\n");
 
+//ft_strncpy
+	char str3[10] = "saluttoi";
+	char str4[7] = "coucou";
+	printf("ft_strncpy : %s\n", ft_strncpy(str3, str4, 4));
+	char str3b[10] = "saluttoi";
+	printf("strncpy : %s\n", strncpy(str3b, str4, 4));
+	printf("--------------------------------\n");
+
+//ft_strstr
+	char str5[10] = "saluttoi";
+	char str6[9] = "lut";
+	printf("ft_strstr(%s, %s) : %s\n", str5, str6, ft_strstr(str5, str6));
+	printf("strstr(%s, %s) : %s\n", str5, str6, strstr(str5, str6));
+	printf("--------------------------------\n");
 
 	return (0);
 }
